3742cc8727ad904787d26e3e0b788723b74ed733
valerija86rg <valerija86rg@gmail.com>
2025-05-05 14:28:56 +0500

    h6: modifier

--- main/task2.sql
+++ main/task2.sql
@@ -5,9 +5,9 @@
 -- Создание платежа
 Declare
    v_message        varchar2(200 char) := 'Платеж создан';
-   c_status_create  constant number(1) := 0;
+   c_status_create  constant payment.status%type := 0;
    v_current_dtime  date := sysdate;
-   v_payment_id     number(38) := 1;
+   v_payment_id     payment.payment_id%type := 1;
 begin
    dbms_output.put_line(v_message||'. Статус: '||c_status_create);
    dbms_output.put_line(to_char(v_current_dtime, 'yyyy/mm/dd hh24:mi:ss'));
@@ -18,10 +18,10 @@ end;
 -- Сброс платежа
 Declare
    v_message       varchar2(200 char) := 'Сброс платежа в "ошибочный статус" с указанием причины';
-   c_status_error  constant number(1) := 2;
-   v_reason        varchar2(200 char) := 'недостаточно средств';
+   c_status_error  constant payment.status%type := 2;
+   v_reason        payment.status_change_reason%type := 'недостаточно средств';
    v_current_dtime timestamp := systimestamp;
-   v_payment_id    number(38) := 1;
+   v_payment_id    payment.payment_id%type := 1;
    v_message_error_id_null     varchar2(100 char) := 'ID объекта не может быть пустым';
    v_message_error_reason_null varchar2(100 char) := 'Причина не может быть пустой';
 begin
@@ -40,10 +40,10 @@ end;
 -- Отмена платежа
 Declare
    v_message       varchar2(200 char):= 'Отмена платежа с указанием причины';
-   c_status_cancel constant number(1) := 3;
-   v_reason        varchar2(200 char):= 'ошибка пользователя';
+   c_status_cancel constant payment.status%type := 3;
+   v_reason        payment.status_change_reason%type:= 'ошибка пользователя';
    v_current_dtime timestamp := systimestamp;
-   v_payment_id    number(38) := 1;
+   v_payment_id    payment.payment_id%type := 1;
    v_message_error_id_null     varchar2(100 char) := 'ID объекта не может быть пустым';
    v_message_error_reason_null varchar2(100 char) := 'Причина не может быть пустой';
 begin
@@ -62,9 +62,9 @@ end;
 -- Завершение платежа
 Declare
    v_message               varchar2(200 char) := 'Успешное завершение платежа';
-   c_status_success        constant number(1) := 1;
+   c_status_success        constant payment.status%type := 1;
    v_current_dtime         date := sysdate;
-   v_payment_id            number(38) := 1;
+   v_payment_id            payment.payment_id%type := 1;
    v_message_error_id_null varchar2(100 char) := 'ID объекта не может быть пустым';
 begin
    dbms_output.put_line(v_message||'. Статус: '||c_status_success);
@@ -80,7 +80,7 @@ end;
 Declare
    v_message               varchar2(200 char) := 'Данные платежа добавлены или обновлены по списку id_поля/значение';
    v_current_dtime         date := sysdate;
-   v_payment_id            number(38) := 1;
+   v_payment_id            payment.payment_id%type := 1;
    v_message_error_id_null varchar2(100 char) := 'ID объекта не может быть пустым';
 begin
    dbms_output.put_line(v_message);
@@ -96,7 +96,7 @@ end;
 Declare
    v_message               varchar2(200 char) := 'Детали платежа удалены по списку id_полей';
    v_current_dtime         timestamp := systimestamp;
-   v_payment_id            number(38) := 1;
+   v_payment_id            payment.payment_id%type := 1;
    v_message_error_id_null varchar2(100 char) := 'ID объекта не может быть пустым';
 begin
    dbms_output.put_line(v_message);

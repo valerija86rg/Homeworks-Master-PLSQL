7cbcb5aeb12c4950716c542bc221ee4a3f0f241b
valerija86rg <valerija86rg@gmail.com>
2025-05-28 21:31:03 +0500

    hw14: exceptions

--- main/payment_detail_api_pack.pks
+++ main/payment_detail_api_pack.pks
@@ -4,9 +4,20 @@ create or replace package payment_detail_api_pack  is
 Описание пакета: API для сущности “Детали платежа”
 */
   c_err_msg_empty_object_id   constant varchar2(100 char) := 'ID объекта не может быть пустым';
+  c_err_msg_empty_field_id    constant varchar2(100 char) := 'ID поля не может быть пустым';
+  c_err_msg_empty_field_value constant varchar2(100 char) := 'Значение в поле не может быть пустым';
+  c_err_msg_empty_collection  constant varchar2(100 char) := 'Коллекция не содержит данных';
   
   c_info_msg_delete_payment_detail       constant varchar2(100 char) := 'Детали платежа удалены по списку id_полей';
   c_info_msg_update_payment_detail       constant varchar2(200 char) := 'Данные платежа добавлены или обновлены по списку id';
+
+   --коды ошибок
+  c_error_code_invalid_input_parametr    constant number(10) := -20101;
+  
+  --объекты исключений
+  e_invalid_input_parametr exception;
+  pragma exception_init(e_invalid_input_parametr, -20101);
+  
 
   /*
   *  Добавление или обновление данных по платежу
@@ -24,4 +35,3 @@ create or replace package payment_detail_api_pack  is
   procedure delete_payment_detail(p_delete_field_ids t_number_array,
                                   p_payment_id       payment.payment_id%type);
 end payment_detail_api_pack;
-/
--- main/unit_test.sql
+++ main/unit_test.sql
@@ -90,3 +90,94 @@ end;
   where pd.payment_id = 21
     and pd.field_id = dpf.field_id
   order by pd.field_id;
+  
+--негативные Unit тесты
+--Проверка "Создание платежа"
+declare 
+  v_payment_detail t_payment_detail_array;
+  v_summa           payment.summa%type; 
+  v_currency_id     currency.currency_id%type;
+  v_from_client_id  client.client_id%type;
+  v_to_client_id    client.client_id%type;
+  v_create_dtime    timestamp := systimestamp;
+  v_payment_id      payment.payment_id%type;
+begin 
+  v_payment_id := payment_api_pack.create_payment(p_summa => v_summa, 
+                                                  p_currency_id => v_currency_id,
+                                                  p_from_client_id => v_from_client_id, 
+                                                  p_to_client_id => v_to_client_id, 
+                                                  p_create_dtime => v_create_dtime,
+                                                  p_payment_detail => v_payment_detail);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Создание платежа. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
+
+--Проверка "Сброс платежа"  
+declare 
+  v_reason        payment.status_change_reason%type;
+  v_payment_id    payment.payment_id%type;
+begin
+  payment_api_pack.fail_payment(p_payment_id => v_payment_id,
+                                p_reason => v_reason);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Сброс платежа. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
+
+--Проверка "Отмена платежа" 
+declare 
+  v_reason        payment.status_change_reason%type;
+  v_payment_id    payment.payment_id%type;
+begin  
+  payment_api_pack.cancel_payment(p_payment_id => v_payment_id,
+                                  p_reason => v_reason);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Отмена платежа. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
+
+--Проверка "Завершение платежа" 
+declare 
+  v_payment_id     payment.payment_id%type;
+begin
+  payment_api_pack.successful_finish_payment(p_payment_id => v_payment_id);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Завершение платежа. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
+
+--Проверка "Добавление или обновление данных по платежу" 
+declare 
+  v_payment_id      payment.payment_id%type;
+  v_payment_detail  t_payment_detail_array;
+begin
+  payment_detail_api_pack.insert_or_update_payment_detail(p_payment_detail => v_payment_detail,
+                                                          p_payment_id => v_payment_id);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_detail_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Добавление или обновление данных по платежу. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
+--Проверка "Удаление деталей платежа"
+declare
+  v_payment_id            payment.payment_id%type;
+  v_delete_field_ids      t_number_array;
+begin
+  payment_detail_api_pack.delete_payment_detail(p_delete_field_ids => v_delete_field_ids,
+                                                p_payment_id => v_payment_id);
+  raise_application_error(-20999, 'Unit-тест или API выполнены неверно');
+  exception
+    when payment_detail_api_pack.e_invalid_input_parametr then 
+      dbms_output.put_line('Удаление деталей платежа. Исключение возбуждено успешно. Ошибка: '||sqlerrm);
+end;
+/
